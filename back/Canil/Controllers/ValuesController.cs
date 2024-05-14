1a07fec9a1bd9f568789d8a460221e93497002d6
LUCAS JUNIO ALVES VELOSO DOS REIS <lucasjunioreisdev@gmail.com>
2024-05-13 22:13:48 -0300

    Projeto finalizado

---
+++ back/Canil/Controllers/ValuesController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Canil.Models;
+
+namespace Canil.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private CanilModel canil1 = new CanilModel("Meu Canil Feliz", 2, 20, 40, 22, 48);
+        private CanilModel canil2 = new CanilModel("Vai Rex", 1.7f, 15, 50, 20, 55);
+        private CanilModel canil3 = new CanilModel("Chow Chawgas", 0.8f, 30, 45, 30, 45);
+
+        [HttpPost(Name = "getUserInput")]
+        public string GetUserInput(DateTime data, int caesPequenos, int caesGrandes)
+        {
+            List<CanilModel> canis = new List<CanilModel> { canil1, canil2, canil3 };
+
+            bool fimDeSemana = validarDiaDaSemana(data);
+            float totalCanil1 = canil1.GetPrice(caesPequenos, caesGrandes, fimDeSemana);//Devolver o valor total do banho p/ pets P,G
+            float totalCanil2 = canil2.GetPrice(caesPequenos, caesGrandes, fimDeSemana);
+            float totalCanil3 = canil3.GetPrice(caesPequenos, caesGrandes, fimDeSemana);
+            int melhorValorCanil = validarMelhorValor(totalCanil1, totalCanil2, totalCanil3);
+            return ($"O melhor petshop é o {canis[melhorValorCanil].nome} e o valor é {canis[melhorValorCanil].GetPrice(caesPequenos, caesGrandes, fimDeSemana)}");
+        }
+
+        private bool validarDiaDaSemana(DateTime dataDoBanho)
+        {
+            DateTime hoje = DateTime.Today;
+            if (hoje.DayOfWeek == DayOfWeek.Saturday || hoje.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+ 
+        //                                         10              20               20
+        private int validarMelhorValor(float totalCanil1, float totalCanil2, float totalCanil3)
+        {
+            if (totalCanil1 < totalCanil2 && totalCanil1 < totalCanil3)
+            {
+                return 0;
+            }
+            else if (totalCanil2 < totalCanil1 && totalCanil2 < totalCanil3)
+            {
+                return 1;
+            }
+            else if (totalCanil3 < totalCanil1 && totalCanil3 < totalCanil2)
+            {
+                return 2;
+            }
+            else
+            {
+                return getMenorDistancia(totalCanil1, totalCanil2, totalCanil3);
+            }
+        }
+
+        private int getMenorDistancia(float totalCanil1, float totalCanil2, float totalCanil3)
+        {
+            if (totalCanil1 == totalCanil2) 
+            { 
+                return canil1.distancia < canil2.distancia ? 0 : 1; 
+            }
+            else if (totalCanil1 == totalCanil3) 
+            {
+                return canil1.distancia < canil3.distancia ? 0 : 2; 
+            }
+            else 
+            { 
+                return canil2.distancia < canil3.distancia ? 1 : 2; 
+            }
+        }
+
+    }
+}
---
+++ back/Canil/Models/Canil.cs
@@ -0,0 +1,36 @@
+﻿namespace Canil.Models;
+
+public class CanilModel
+{
+    public string nome;
+    public float distancia;
+    public float valorDiaSemanaPequeno;
+    public float valorDiaSemanaGrande;
+    public float valorFdsPequeno;
+    public float valorFdsGrande;
+
+    public float GetPrice(int smallDogs, int bigDogs, bool isWeekend)
+    {
+        if (isWeekend)
+        {
+
+            return valorFdsPequeno * smallDogs + valorFdsGrande * bigDogs; //Valor total dos banhos fds
+        }
+        else
+        {
+
+            return valorDiaSemanaPequeno * smallDogs + valorDiaSemanaGrande * bigDogs;//Valor total dos banhos semana
+        }
+    }
+
+    public CanilModel(string nome, float distancia, float valorDiaSemanaPequeno, float valorDiaSemanaGrande, float valorFdsPequeno, float valorFdsGrande)
+    {
+        this.nome = nome;
+        this.distancia = distancia;
+        this.valorDiaSemanaPequeno = valorDiaSemanaPequeno;
+        this.valorDiaSemanaGrande = valorDiaSemanaGrande;
+        this.valorFdsPequeno = valorFdsPequeno;
+        this.valorFdsGrande = valorFdsGrande;
+    }
+}
+
